c6ade6bc3071857c20a1c6f002e6f96a65805278
siddsy <sidnykblore560047@gmail.com>
2026-03-18 13:33:56 +1300

    Add Docker deployment files: Dockerfile, docker-compose.yml, nginx.conf

--- drivingjobs/settings.py
+++ drivingjobs/settings.py
@@ -109,6 +109,9 @@ STORAGES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
